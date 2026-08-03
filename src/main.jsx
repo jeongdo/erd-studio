cf4a5f10c94b76818001835b260f03d019bdbb33
jeongdo <me.jeongdo@gmail.com>
2026-08-04 00:08:31 +0900

    chore: migrate styling to Tailwind CSS with Vite build environment

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+// Currently, React is just initializing the environment.
+// The actual DOM rendering is handled by the legacy app.js.
+// Future React components can be mounted here.
+console.log('React + Vite + Tailwind Environment Initialized');
