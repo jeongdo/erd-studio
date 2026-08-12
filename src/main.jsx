85b784970948a90e0dd585c148bf0479bc6385aa
jeongdo <me.jeongdo@gmail.com>
2026-08-12 16:20:32 +0900

    chore: load drag click guard

--- src/main.jsx
+++ src/main.jsx
@@ -21,6 +21,7 @@ function loadClassic(src, marker, onload) {
 }
 
 function loadProjectLayer() {
+  loadClassic('/editor-drag-ux.js', 'drag-ux')
   loadClassic('/editor-project.js', 'project', () => {
     loadClassic('/editor-project-dock.js', 'project-dock', () => {
       loadClassic('/editor-project-dock-ux.js', 'project-dock-ux', () => {
