52bbd947e074eb9ab4268c537f5a90165c0177e4
jeongdo <me.jeongdo@gmail.com>
2026-08-13 23:15:26 +0900

    test: remove temporary branch validation marker [skip ci]

--- tests/folder-project-branch-validation.test.mjs
+++
@@ -1,7 +0,0 @@
-import test from 'node:test'
-import assert from 'node:assert/strict'
-import fs from 'node:fs'
-
-test('folder-project branch validation includes the action override', () => {
-  assert.equal(fs.existsSync(new URL('../editor-folder-project-actions.js', import.meta.url)), true)
-})
